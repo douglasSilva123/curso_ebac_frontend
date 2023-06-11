0a26b4dc2a4e849aa7cd098fb194cdb775a58ce3
DOUGLAS PAULINO <115375067+douglasSilva123@users.noreply.github.com>
2023-06-11 11:55:17 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,16 @@
-const form = document.getElementById('form-deposito');
-
-function validaNome(nomeCompleto){
-   const nomeComoArray = nomeCompleto.split(' ');
-   return nomeComoArray.length >= 2;
-}
-
-form.addEventListener('submit',function(e){
-    let formEvalido = false;
-    e.preventDefault();
-
-    const nomeBeneficiario = document.getElementById('nome-beneficiario');
-    const numeroContaBeneficiario = document.getElementById('numero-conta');
-    const valorDeposito = document.getElementById('valor-deposito');
-    const mensagemSucesso = 'Montante de: ${valorDeposito.value} depositado para o cliente: ${nomeBeneficiario} - conta:${numeroContaBeneficiario}';
-
-
-    formEvalido = validaNome(nomeBeneficiario.value)
-    if (formEvalido){
-        alert("mensagemSucesso");
+function validarFormulario() {
+    var campoA = parseInt(document.getElementById("campoA").value);
+    var campoB = parseInt(document.getElementById("campoB").value);
+    var mensagem = document.getElementById("mensagem");
+    if(campoB > campoA){
+        mensagem.textContent = "Formulário válido!B é maior que A.";
+        mensagem.classList.add("positiva");
+        mensagem.classList.remove("negativa");
     }else{
-        alert("O nome não esta completo");
+        mensagem.textContent = "Formulário inválido!B deve ser maior que A.";
+        mensagem.classList.add("negativa");
+        menseger.classList.remove("positiva");
     }
-})
 
-console.log(form);
+    return false;
+}
